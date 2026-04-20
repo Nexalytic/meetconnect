07f2a76805a7535580d72e257660a2f4cd73ae7f
Nexalytic <phoenix81526@gmail.com>
2026-04-20 10:17:30 +0530

    Update User.js

--- server/models/User.js
+++ server/models/User.js
@@ -7,20 +7,28 @@ const userSchema = new mongoose.Schema(
       required: true,
       trim: true,
     },
+
     email: {
       type: String,
       required: true,
       unique: true,
       lowercase: true,
     },
+
     password: {
       type: String,
       required: true,
       minlength: 6,
     },
+
     phone: {
       type: String,
       required: true,
+    },
+
+    dob: {
+      type: String,
+      default: "",
     },
   },
   { timestamps: true }
